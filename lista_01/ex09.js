39015735695c3f93296e113783744fea4602b05c
rayanebrugnagosilva-creator <rayane.brugnago.silva@escola.pr.gov.br>
2026-06-21 23:56:23 -0300

    Create ex09.js

---
+++ lista_01/ex09.js
@@ -0,0 +1,26 @@
+function min(a, b) {
+
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+
+}
+
+console.log(min(10, 5));
+
+
+// Arrow Function
+
+let minArrow = (a, b) => {
+
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+
+}
+
+console.log(minArrow(8, 3));
